e50575516256bea172df67ca9dba38028e27fbad
jaxxx-exe <juarezlopezkevin95@gmail.com>
2026-06-06 23:47:35 -0600

    Update Principal.java
    se realiza la clase principal para poder probar el funcionamiento de los hilos

--- src/hilos/Principal.java
+++ src/hilos/Principal.java
@@ -15,6 +15,17 @@ public class Principal {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        Cuenta cuenta1 = new Cuenta(100); //se crea la cuenta unica
+        
+        retiro retiro1 = new retiro(cuenta1, "Cajero banorte", 70); //se realiza el primer retiro
+        retiro retiro2 = new retiro(cuenta1, "Cajero banorte 2", 70);//se realiza el segundo retiro
+        
+        Thread hilo1 = new Thread(retiro1);//Se asigna cada retiro a un hilo
+        Thread hilo2 = new Thread(retiro2);
+        
+        hilo1.start();//se inician los hilos
+        hilo2.start();
     }
     
 }
